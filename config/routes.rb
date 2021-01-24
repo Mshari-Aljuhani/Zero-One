bcadba01c5e174064a999f23ca381eed185563de
Mshari <76810980+DreamTeam77@users.noreply.github.com>
2021-01-24 21:05:46 +0300

    fix reg system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+    root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
