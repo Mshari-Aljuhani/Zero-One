168e20f0e2b13cbcf6831206556b315ce284fed3
Mshari <76810980+DreamTeam77@users.noreply.github.com>
2021-02-04 04:56:56 +0300

    تغيير صفحة البداية لصفحة المواضيع

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+
   resources :questions
   devise_for :users
   devise_scope :user do
+
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-    root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'questions#index'
 end
 
